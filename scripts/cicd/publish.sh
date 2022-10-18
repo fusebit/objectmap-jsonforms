bf78ac529f28b8d69dd0fada8b74ba4d070c9b77
MikeFuster <mfuster@litebox.ai>
2022-10-18 16:21:32 -0300

    fix scripts

--- scripts/cicd/publish.sh
+++ scripts/cicd/publish.sh
@@ -8,5 +8,7 @@ set -x
 echo "Creating ${HOMEDIR}/.npmrc"
 echo //registry.npmjs.org/:_authToken=${SECRET_NPM_TOKEN} > ${HOMEDIR}/.npmrc
 
-cd src/react-marketplace/
+cd src/objectmap-jsonforms/
+npm publish
+cd ../objectmap-utils
 npm publish
